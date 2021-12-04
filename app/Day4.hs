d6d307d09321fac676dafb18a6e034ba6539b3f6
Stoephu <christoph.straehl@gmx.chu>
2021-12-04 15:38:56 +0100

    Day4 done

--- app/Day4.hs
+++ app/Day4.hs
@@ -1,14 +1,15 @@
---import Debug.Trace
+import Debug.Trace
 
 main :: IO ()
 main = do
-    contents <- readFile "inputs/day4.txt"
+    contents <- readFile "inputs/Day4.txt"
     let listOfLine = lines contents
     let replacerCom = replace (== ',') ' '
     let pulls = map read $ wordsWhen replacerCom $ head listOfLine :: [Int]
     let boardints = map (map read . wordsWhen replacerCom) (tail $ tail listOfLine) :: [[Int]]
     print pulls
     print $ play (map addTransposeBoard $ groupBoards boardints) pulls 
+    print $ play2 (map addTransposeBoard $ groupBoards boardints) pulls 
     
     -- part due
 
@@ -37,6 +38,7 @@ pop :: Int -> [Int] -> [Int]
 pop _ [] = []
 pop y (x:xs) = if x == y then xs else x: pop y xs
 
+merge :: [a] -> [a] -> [a]
 merge [] ys = ys
 merge (x:xs) ys = x:merge ys xs
 
@@ -44,3 +46,11 @@ play :: [[[Int]]] -> [Int] -> Int
 play boards (p:pulls) = if null winningBoards then play nextBoards pulls else div (sum $ map sum (head winningBoards) ) 2 * p
     where   nextBoards = map (map (pop p)) boards
             winningBoards = filter (any null) nextBoards
+play _ _ = error "go away"
+
+play2 :: [[[Int]]] -> [Int] -> Int
+play2 boards (p:pulls) = trace (show p ++ " \n " ++ show currentLosingBoards ++"\n" ++ show nextLosingBoards ) (if (length currentLosingBoards == 1) && null nextLosingBoards then div (sum $ map sum (head currentLosingBoards) ) 2 * p - p * p else play2 nextBoards pulls )
+    where   nextBoards = map (map (pop p)) boards
+            currentLosingBoards = filter (not . any null) boards
+            nextLosingBoards = filter (not . any null) nextBoards
+play2 _ _ = error "go away"
