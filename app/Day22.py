dd564eda2fe37374ab36db013974856a00d4fcc1
Stoephu <christoph.straehl@gmx.ch>
2022-01-15 18:41:53 +0100

    ffffffffffff

--- app/Day22.py
+++ app/Day22.py
@@ -2,7 +2,8 @@
 from collections import namedtuple
 
 Command = namedtuple('Command', ['flip_on' ,'ranges'])
-Range = namedtuple('Range', ['xs', 'ys', 'zs'])
+Range = namedtuple('Range', ['x', 'y', 'z'])
+Coord = namedtuple('Coord', ['anchor', 'size'])
 #%%
 def command(line):
     flip_string, coords_string = line.split()
@@ -10,100 +11,91 @@ def command(line):
     if flip_string == "on":
         flip_on = True
     coords_string = coords_string.replace('=',',').replace('..',',').split(',')
-    x_range = int(coords_string[1]),int(coords_string[2]) +1 
-    y_range = int(coords_string[4]),int(coords_string[5]) +1
-    z_range = int(coords_string[7]),int(coords_string[8]) +1
+    x_range = Coord(int(coords_string[1]),int(coords_string[2]) -int(coords_string[1]) +1) #if flip_on else int(coords_string[2])
+    y_range = Coord(int(coords_string[4]),int(coords_string[5]) -int(coords_string[4]) +1) #if flip_on else int(coords_string[5])
+    z_range = Coord(int(coords_string[7]),int(coords_string[8]) -int(coords_string[7]) +1) #if flip_on else int(coords_string[8])
     return Command(flip_on,Range(x_range,y_range,z_range)) 
 #%%
 inp =  [line.strip() for line in open('../inputs/small_day22.txt')]
-commands = [command(line) for line in inp][:3][::-1]
+commands = [command(line) for line in inp][::-1]
 
 # %%
+def cube_num(ranges):
+    x,y,z = ranges
+    return (x.size)*(y.size)*(z.size)
+
+def ena(coord: Coord): # end_neighbour_anchor
+    return coord.anchor + coord.size
+
 def inside(o_range,i_range) -> bool:
-    min_val, max_val = i_range
-    if min_val < o_range[1] and min_val >= o_range[0]:
-        return True
-    if max_val > o_range[0] and max_val <= o_range[1]:
+    i_anchor, i_size = i_range
+    o_anchor, o_size = o_range
+    if ena(i_range) - 1 >= o_anchor and i_anchor - o_anchor <= o_size - 1:
         return True
     return False
-def get_overlap_range(o_range,i_range):
-    new = []
-    new.append(i_range[0] if i_range[0] > o_range[0] else o_range[0])
-    new.append(i_range[1] if i_range[1] < o_range[1] else o_range[1])
+
+def get_overlap_coord(o_range,i_range):
+    anchor = i_range.anchor if i_range.anchor >= o_range.anchor else o_range.anchor
+    size = ena(i_range) - anchor if ena(i_range) <= ena(o_range) else ena(o_range) - anchor
+    new = Coord(anchor, size)
     return new
 
 def overlap(ranges1,ranges2) -> list:
-    xs1,xs2 = ranges1.xs,ranges2.xs
-    ys1,ys2 = ranges1.ys,ranges2.ys
-    zs1,zs2 = ranges1.zs,ranges2.zs
+    x1,x2 = ranges1.x,ranges2.x
+    y1,y2 = ranges1.y,ranges2.y
+    z1,z2 = ranges1.z,ranges2.z
     # exit with None if no overlap
     new_block = []
-    if inside(xs1,xs2):
-        new_block.append(get_overlap_range(xs1,xs2))
-    elif inside(xs2,xs1):
-        new_block.append(get_overlap_range(xs2,xs1))
+    if inside(x1,x2):
+        new_block.append(get_overlap_coord(x1,x2))
     else:
         return None
     
-    if inside(ys1,ys2):
-        new_block.append(get_overlap_range(ys1,ys2))
-    elif inside(ys2,ys1):
-        new_block.append(get_overlap_range(ys2,ys1))
+    if inside(y1,y2):
+        new_block.append(get_overlap_coord(y1,y2))
     else:
         return None
 
-    if inside(zs1,zs2):
-        new_block.append(get_overlap_range(zs1,zs2))
-    elif inside(zs2,zs1):
-        new_block.append(get_overlap_range(zs2,zs1))
+    if inside(z1,z2):
+        new_block.append(get_overlap_coord(z1,z2))
     else:
         return None
     return Range(*new_block)
     
-def cube_num(ranges):
-    xs,ys,zs = ranges
-    return (xs[1]-xs[0])*(ys[1]-ys[0])*(zs[1]-zs[0])
 # %%
 def split_cube_from_off(on_range,off_range):
     compact_off = overlap(on_range,off_range)
     if not compact_off:
         return None
-    top_cube = Range((compact_off.xs[1],on_range.xs[1]),compact_off.ys,compact_off.zs)
-    bottom_cube = Range((on_range.xs[0],compact_off.xs[0]),compact_off.ys,compact_off.zs)
-    front_cube = Range(compact_off.xs,(compact_off.ys[1],on_range.ys[1]),compact_off.zs)
-    back_cube = Range(compact_off.xs,(on_range.ys[0],compact_off.ys[0]),compact_off.zs)
-    left_cube = Range(compact_off.xs,compact_off.ys,(on_range.zs[0],compact_off.zs[0]))
-    right_cube = Range(compact_off.xs,compact_off.ys,(compact_off.zs[1],on_range.zs[1]))
-    t_f_edge = Range((compact_off.xs[1],on_range.xs[1]),(compact_off.ys[1],on_range.ys[1]),compact_off.zs)
-    t_b_edge = Range((compact_off.xs[1],on_range.xs[1]),(on_range.ys[0],compact_off.ys[0]),compact_off.zs)
-    t_l_edge = Range((compact_off.xs[1],on_range.xs[1]),compact_off.ys,(on_range.zs[0],compact_off.zs[0]))
-    t_r_edge = Range((compact_off.xs[1],on_range.xs[1]),compact_off.ys,(compact_off.zs[1],on_range.zs[1]))
-    l_f_edge = Range(compact_off.xs,(compact_off.ys[1],on_range.ys[1]),(on_range.zs[0],compact_off.zs[0]))
-    r_f_edge = Range(compact_off.xs,(compact_off.ys[1],on_range.ys[1]),(compact_off.zs[1],on_range.zs[1])) 
-    l_b_edge = Range(compact_off.xs,(on_range.ys[0],compact_off.ys[0]),(on_range.zs[0],compact_off.zs[0]))
-    r_b_edge = Range(compact_off.xs,(on_range.ys[0],compact_off.ys[0]),(compact_off.zs[1],on_range.zs[1]))
-    b_f_edge = Range((on_range.xs[0],compact_off.xs[0]),(compact_off.ys[1],on_range.ys[1]),compact_off.zs)
-    b_b_edge = Range((on_range.xs[0],compact_off.xs[0]),(on_range.ys[0],compact_off.ys[0]),compact_off.zs)
-    b_l_edge = Range((on_range.xs[0],compact_off.xs[0]),compact_off.ys,(on_range.zs[0],compact_off.zs[0]))
-    b_r_edge = Range((on_range.xs[0],compact_off.xs[0]),compact_off.ys,(compact_off.zs[1],on_range.zs[1]))
-    t_f_l_corner =  Range((compact_off.xs[1],on_range.xs[1]),(compact_off.ys[1],on_range.ys[1]),(on_range.zs[0],compact_off.zs[0]))
-    t_f_r_corner =  Range((compact_off.xs[1],on_range.xs[1]),(compact_off.ys[1],on_range.ys[1]),(compact_off.zs[1],on_range.zs[1]))
-    t_b_l_corner = Range((compact_off.xs[1],on_range.xs[1]),(on_range.ys[0],compact_off.ys[0]),(on_range.zs[0],compact_off.zs[0]))
-    t_b_r_corner = Range((compact_off.xs[1],on_range.xs[1]),(on_range.ys[0],compact_off.ys[0]),(compact_off.zs[1],on_range.zs[1]))
-    b_f_l_corner =  Range((on_range.xs[0],compact_off.xs[0]),(compact_off.ys[1],on_range.ys[1]),(on_range.zs[0],compact_off.zs[0]))
-    b_f_r_corner =  Range((on_range.xs[0],compact_off.xs[0]),(compact_off.ys[1],on_range.ys[1]),(compact_off.zs[1],on_range.zs[1]))
-    b_b_l_corner = Range((on_range.xs[0],compact_off.xs[0]),(on_range.ys[0],compact_off.ys[0]),(on_range.zs[0],compact_off.zs[0]))
-    b_b_r_corner = Range((on_range.xs[0],compact_off.xs[0]),(on_range.ys[0],compact_off.ys[0]),(compact_off.zs[1],on_range.zs[1]))
+    top_cube = Range(Coord(ena(compact_off.x),ena(on_range.x)-ena(compact_off.x)),compact_off.y,compact_off.z)
+    bottom_cube = Range(Coord(on_range.x.anchor, compact_off.x.anchor - on_range.x.anchor),compact_off.y,compact_off.z)
+    front_cube = Range(compact_off.x,Coord(ena(compact_off.y),ena(on_range.y)-ena(compact_off.y)),compact_off.z)
+    back_cube = Range(compact_off.x,Coord(on_range.y.anchor, compact_off.y.anchor - on_range.y.anchor),compact_off.z)
+    right_cube = Range(compact_off.x,compact_off.y,Coord(ena(compact_off.z),ena(on_range.z)-ena(compact_off.z)))
+    left_cube = Range(compact_off.x,compact_off.y,Coord(on_range.z.anchor, compact_off.z.anchor - on_range.z.anchor))
+    t_f_edge = Range(Coord(ena(compact_off.x),ena(on_range.x)-ena(compact_off.x)),Coord(ena(compact_off.y),ena(on_range.y)-ena(compact_off.y)),compact_off.z)
+    t_b_edge = Range(Coord(ena(compact_off.x),ena(on_range.x)-ena(compact_off.x)),Coord(on_range.y.anchor, compact_off.y.anchor - on_range.y.anchor),compact_off.z)
+    t_r_edge = Range(Coord(ena(compact_off.x),ena(on_range.x)-ena(compact_off.x)),compact_off.y,Coord(ena(compact_off.z),ena(on_range.z)-ena(compact_off.z)))
+    t_l_edge = Range(Coord(ena(compact_off.x),ena(on_range.x)-ena(compact_off.x)),compact_off.y,Coord(on_range.z.anchor, compact_off.z.anchor - on_range.z.anchor))
+    r_f_edge = Range(compact_off.x,Coord(ena(compact_off.y),ena(on_range.y)-ena(compact_off.y)),Coord(ena(compact_off.z),ena(on_range.z)-ena(compact_off.z))) 
+    l_f_edge = Range(compact_off.x,Coord(ena(compact_off.y),ena(on_range.y)-ena(compact_off.y)),Coord(on_range.z.anchor, compact_off.z.anchor - on_range.z.anchor))
+    r_b_edge = Range(compact_off.x,Coord(on_range.y.anchor, compact_off.y.anchor - on_range.y.anchor),Coord(ena(compact_off.z),ena(on_range.z)-ena(compact_off.z)))
+    l_b_edge = Range(compact_off.x,Coord(on_range.y.anchor, compact_off.y.anchor - on_range.y.anchor),Coord(on_range.z.anchor, compact_off.z.anchor - on_range.z.anchor))
+    b_f_edge = Range(Coord(on_range.x.anchor, compact_off.x.anchor - on_range.x.anchor),Coord(ena(compact_off.y),ena(on_range.y)-ena(compact_off.y)),compact_off.z)
+    b_b_edge = Range(Coord(on_range.x.anchor, compact_off.x.anchor - on_range.x.anchor),Coord(on_range.y.anchor, compact_off.y.anchor - on_range.y.anchor),compact_off.z)
+    b_r_edge = Range(Coord(on_range.x.anchor, compact_off.x.anchor - on_range.x.anchor),compact_off.y,Coord(ena(compact_off.z),ena(on_range.z)-ena(compact_off.z)))
+    b_l_edge = Range(Coord(on_range.x.anchor, compact_off.x.anchor - on_range.x.anchor),compact_off.y,Coord(on_range.z.anchor, compact_off.z.anchor - on_range.z.anchor))
+    t_f_r_corner =  Range(Coord(ena(compact_off.x),ena(on_range.x)-ena(compact_off.x)),Coord(ena(compact_off.y),ena(on_range.y)-ena(compact_off.y)),Coord(ena(compact_off.z),ena(on_range.z)-ena(compact_off.z)))
+    t_f_l_corner =  Range(Coord(ena(compact_off.x),ena(on_range.x)-ena(compact_off.x)),Coord(ena(compact_off.y),ena(on_range.y)-ena(compact_off.y)),Coord(on_range.z.anchor, compact_off.z.anchor - on_range.z.anchor))
+    t_b_r_corner = Range(Coord(ena(compact_off.x),ena(on_range.x)-ena(compact_off.x)),Coord(on_range.y.anchor, compact_off.y.anchor - on_range.y.anchor),Coord(ena(compact_off.z),ena(on_range.z)-ena(compact_off.z)))
+    t_b_l_corner = Range(Coord(ena(compact_off.x),ena(on_range.x)-ena(compact_off.x)),Coord(on_range.y.anchor, compact_off.y.anchor - on_range.y.anchor),Coord(on_range.z.anchor, compact_off.z.anchor - on_range.z.anchor))
+    b_f_r_corner =  Range(Coord(on_range.x.anchor, compact_off.x.anchor - on_range.x.anchor),Coord(ena(compact_off.y),ena(on_range.y)-ena(compact_off.y)),Coord(ena(compact_off.z),ena(on_range.z)-ena(compact_off.z)))
+    b_f_l_corner =  Range(Coord(on_range.x.anchor, compact_off.x.anchor - on_range.x.anchor),Coord(ena(compact_off.y),ena(on_range.y)-ena(compact_off.y)),Coord(on_range.z.anchor, compact_off.z.anchor - on_range.z.anchor))
+    b_b_r_corner = Range(Coord(on_range.x.anchor, compact_off.x.anchor - on_range.x.anchor),Coord(on_range.y.anchor, compact_off.y.anchor - on_range.y.anchor),Coord(ena(compact_off.z),ena(on_range.z)-ena(compact_off.z)))
+    b_b_l_corner = Range(Coord(on_range.x.anchor, compact_off.x.anchor - on_range.x.anchor),Coord(on_range.y.anchor, compact_off.y.anchor - on_range.y.anchor),Coord(on_range.z.anchor, compact_off.z.anchor - on_range.z.anchor))
     cubes =  [top_cube,bottom_cube,front_cube,back_cube,left_cube,right_cube]
     edges = [t_f_edge,t_b_edge,t_l_edge,t_r_edge,l_f_edge,r_f_edge,l_b_edge,r_b_edge,b_f_edge,b_b_edge,b_l_edge,b_r_edge]
-    corners = [t_f_l_corner,
-    t_f_r_corner,
-    t_b_l_corner,
-    t_b_r_corner,
-    b_f_l_corner,
-    b_f_r_corner,
-    b_b_l_corner,
-    b_b_r_corner]
+    corners = [t_f_l_corner,t_f_r_corner,t_b_l_corner,t_b_r_corner,b_f_l_corner,b_f_r_corner,b_b_l_corner,b_b_r_corner]
     cubes.extend(edges)
     cubes.extend(corners)
     non_zero_cubes =  [cube for cube in cubes if cube_num(cube)]
@@ -122,6 +114,9 @@ def resolve_off(commands):
         if not com.flip_on:
             for j, com_j in enumerate(next):
                 if com_j.flip_on:
+                    overlap_cube = overlap(com.ranges,com_j.ranges)
+                    if not overlap_cube:
+                        continue
                     new_cubes = split_cube_from_off(com_j.ranges, com.ranges)
                     if not new_cubes:
                         continue
@@ -163,7 +158,7 @@ def resolve_on_commands(input):
 # %%
 # part 1 
 def count_cubes_init(unique_on):
-    initialitation_cube = Range((-50,50),(-50,50),(-50,50))
+    initialitation_cube = Range(Coord(-50,101),Coord(-50,101),Coord(-50,101))
     num_on_cube = 0
     for com in unique_on:
         flip_on, cube = com
@@ -176,4 +171,28 @@ count_cubes_init(only_on)
 # %%
 unique_on = resolve_on_commands(only_on)
 count_cubes_init(unique_on)
+# %%
+Off = Command(False,Range(Coord(9,3),Coord(9,3),Coord(9,3)))
+On_2 = Command(True, Range(Coord(11,3),Coord(11,3),Coord(11,3)))
+On_1 = Command(True, Range(Coord(10,3),Coord(10,3),Coord(10,3)))
+def test(new_com = None):
+    commands = [Off,On_2,On_1] if not new_com else new_com
+    return count_cubes_init(resolve_off(commands))
+
+def test2(new_com = None):
+    commands = [Off,On_2,On_1] if not new_com else new_com
+    return count_cubes_init(resolve_on_commands(commands))
+def list_all_coords(commands):
+    l = list()
+    for command in commands:
+        ranges = command.ranges
+        x,y,z = ranges
+        for i in range(x.anchor,x.anchor + x.size):
+            for j in range(y.anchor,y.anchor + y.size):
+                for k in range(z.anchor,z.anchor + z.size):
+                    l.append((i,j,k))
+    l.sort()
+    return l
+# %%
+#test()
 # %%
